090ac329fa81e31f5fef46391f05104ac7478333
Pushpak <puspak9208@gmail.com>
2023-06-06 06:42:29 +0530

    variables

--- hello-world/hello_world.go
+++
@@ -1,6 +0,0 @@
-package greeting
-
-// HelloWorld greets the world.
-func HelloWorld() string {
-	return "Goodbye, Mars!"
-}
---
+++ variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt" 
+
+func main(){
+	var a int
+	fmt.Println(a);
+
+	var b int = 4
+	fmt.Println(b)
+
+	var c, d int = 5, 6 
+	fmt.Println(c, d)
+
+	var e= "hello variables"
+	fmt.Println(e)
+
+	f := "declared and initialized !"
+	fmt.Println(f)
+}
